fe84072385a5a6f9e836443c8f65aa9b721f3d04
Swarnim Mukati <swarnim.Mukati@mountblue.tech>
2020-10-09 16:22:53 +0530

    make some changes in index.js file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./routes/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.post("/add", (req, res) => {
